24a09e0677aacb3ccb672cccff060e469e160650
Alan Barzilay <alan.barzilay@gmail.com>
2023-09-10 21:05:58 -0300

    Fix routing/paths for css, imgs, i18n, js, html
    Due to how github pages work, there is a need to add the "base" astro
    parameter to have css imgs and js files to be properly referenced.
    
    Additionally, the base html tag and the "site" astro package were used
    to fix how routing works (relative paths more specifically).
    I couldn't replicate what others did online
    with the base html tag so maybe there are other ways (possibly better?)
    to do this. Astro 3.0 has just been released so this may be a factor
    (but I couldn't find any mention about relevant changes to this
    subject).
    
    Besides that, the "translatepath" i18next-astro function allows links to
    go to their appropriate locale, without always going to the default
    english page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next(), prefetch()]
+  integrations: [tailwind(), astroI18next(), prefetch()],
+  site: 'https://alan-barzilay.github.io',
+  base: 'sidon/',
 });
