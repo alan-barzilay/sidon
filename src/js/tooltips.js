2c3acba65363a8a1736b42d2a4376d69a609edfb
Alan Barzilay <alan.barzilay@gmail.com>
2023-09-12 00:28:42 -0300

    fix cemetery plan svg tooltip photos
    all tooltips were displaying the no photo image for all tombs because
    the way we were building the path to those images and index files was
    wrong. This commit fixes this.

--- src/js/tooltips.js
+++ src/js/tooltips.js
@@ -2,26 +2,8 @@ import tippy from 'tippy.js';
 import 'tippy.js/dist/tippy.css';
 import { create_carousel } from './carousel.js';
 
-// export async function create_tooltip(id){
-//     let imgs = await parse_index(id);
-//     let content;
-//     if(imgs){
-//         content = create_carousel(imgs);
-//     }
-//     else{
-//         content = '<img width="128" src="no-picture.svg" alt="Picture not available. Illustration of a crossed off camera">';
-//     }
-//     return tippy(`#${id}`, {
-//     content: content,
-//     allowHTML: true,
-//     interactive: true,
-//     maxWidth: 350,
-//     appendTo: document.body,
-//     delay: [null, 250], // show default delay and hide 250ms delay
-//     });
-//     }
 
-export async function create_tooltip(id){
+export async function create_tooltip(id, url){
     return tippy(`#${id}`, {
     content: 'Loading...',
     allowHTML: true,
@@ -34,7 +16,7 @@ export async function create_tooltip(id){
         let content;
 
         if(imgs){
-            content = create_carousel(imgs);
+            content = create_carousel(imgs, url);
         }
         else{
             content = '<img width="128" src="no-picture.svg" alt="Picture not available. Illustration of a crossed off camera">';
@@ -46,7 +28,7 @@ export async function create_tooltip(id){
 
 async function parse_index(id){
 
-        let response = await fetch(`${id}/index.html`);
+        let response = await fetch(`by_tomb_id/${id}/index.html`);
         if (!(response.status === 200)) {
             console.log(`id: ${id} returned with http status: ${response.status} `);
             return null }
@@ -59,7 +41,7 @@ async function parse_index(id){
         for (let x of elements) {
             if (regex1.test(x.href)) {
                 index = x.href.lastIndexOf("/");
-                img_srcs.push(x.href.slice(0, index+1) + id + x.href.slice(index) )
+                img_srcs.push(x.href.slice(0, index+1) + "by_tomb_id/" + id + x.href.slice(index) )
             }
         }
         return img_srcs;
