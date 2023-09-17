9ac97081f69e7e6fa177615bb75f72f388838a4e
Alan Barzilay <alan.barzilay@gmail.com>
2023-09-17 15:10:53 -0300

    Set carousel width and reorder hara imgs
    By setting carousel width we avoid tooltip overflow on the svg.
    I reordered the hara images so the first one fully utilizes the space
    available to it
    Lastly, I decreased carousel size in smaller screens to better fit

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -7,7 +7,7 @@ export function create_carousel(imgs, url) {
 function create_carousel_item(img_src, slide_id, N, url){
 	return `
 		<div id=slide${slide_id} class="carousel-item relative w-full">
-		<img src="${img_src}" loading="lazy" class="mx-auto object-contain max-w-96 h-auto" />
+		<img src="${img_src}" loading="lazy" class="mx-auto object-contain w-72 sm:w-96 h-auto" />
 		<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
 			<a href=${url}#slide${ slide_id == 0 ? N-1 : slide_id-1 } class="btn btn-circle">❮</a>
 			<a href=${url}#slide${ slide_id == N-1 ? 0 : slide_id+1 } class="btn btn-circle">❯</a>
