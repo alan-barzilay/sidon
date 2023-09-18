11f1864e3cd6bb462f8245578e859111ac12757c
Alan Barzilay <alan.barzilay@gmail.com>
2023-09-18 18:32:51 -0300

    add un-official robots.txt  plugin.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next(), prefetch(), sitemap()],
+  integrations: [tailwind(), astroI18next(), prefetch(), sitemap(), robotsTxt()],
   site: 'https://nabisaydoun.org/'
 });
