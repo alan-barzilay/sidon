6f57d1786c2e487d26ae00d133e26cf1111547b7
Alan Barzilay <alan.barzilay@gmail.com>
2023-09-18 18:30:32 -0300

    add sitemap plugin. WARNING, will crash on node 20.6.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next(), prefetch()],
-  site: 'https://nabisaydoun.org/',
+  integrations: [tailwind(), astroI18next(), prefetch(), sitemap()],
+  site: 'https://nabisaydoun.org/'
 });
