795e9ca1a2300acba17abe3211a881e84655b768
Alan Barzilay <alan.barzilay@gmail.com>
2023-11-08 14:22:21 -0300

    prefetch: migrate to builtin (v3.5) astro prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
-import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next(), prefetch(), sitemap(), robotsTxt()],
+  integrations: [tailwind(), astroI18next(), sitemap(), robotsTxt()],
+  prefetch: true,
   site: 'https://nabisaydoun.org/'
 });
